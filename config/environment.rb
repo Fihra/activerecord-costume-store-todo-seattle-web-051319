a6c740e8183b61ab2725acdf8c3ec2a5189c047c
Fabian <fabrofabian@gmail.com>
2019-05-27 11:51:59 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 
+require_relative "../config/environment"
+
 require 'rake'
 require 'active_record'
 require 'yaml/store'
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,5 +5,40 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costumes do |c|
+      c.string :name
+      c.integer :price
+      c.integer :size
+      c.string :image_url
+      c.datetime :created_at
+      c.datetime :updated_at
+    end
+
+  end
+
+
+
+
+  # def change
+  #   add_column :costumes, :name, :string
+  #   add_column :costumes, :price, :integer
+  #   add_column :costumes, :size, :integer
+  #   add_column :costumes, :image_url, :string
+  # end
+end
+# class CreateCostumes < ActiveRecord::Migration[5.1]
+#   def change
+#     add_column :costume_stores, :name, :string
+#     add_column :costume_stores, :location, :string
+#     add_column :costume_stores, :number_of_costumes, :integer
+#     add_column :costume_stores, :in_business, :boolean
+#     add_column :costume_stores, :opening_time, :string
+#     add_column :costume_stores, :closing_time, :string
+#   end
+# end
